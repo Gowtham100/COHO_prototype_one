dc9aa1646dbf035a13a21ad0e8019f84595cf5d0
Gowtham <Gowtham>
2017-10-24 10:18:54 -0700

    first commit

---
+++ Assets/Scripts/Instr_Manual.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instr_Manual : MonoBehaviour {
+
+	//public GameObject object3;
+	//public GameObject object4;
+	//public GameObject object6;
+
+	public Transform target;
+	public float speed;
+	public bool rotate = false;
+	public bool start = false;
+
+	// Use this for initialization
+	void Start () {
+		//rotate = false;
+		if (rotate) {
+			transform.Rotate (0, 90, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//object 3 goes to x = 5
+		//object 4 goes to x = 50
+		//object 6 rotates to 0 degs, then moves x = 2, z = 3
+		float step = speed * Time.deltaTime;
+
+		//if (Input.GetKeyDown(KeyCode.A)){
+		if (start) {
+			transform.position = Vector3.MoveTowards (transform.position, target.position, step);
+		}
+		//}
+
+	}
+}
---
+++ Assets/Scripts/Intr_Manager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intr_Manager : MonoBehaviour {
+	
+	public List<GameObject> objects = new List<GameObject>();
+	private Instr_Manual instrM;
+	public GameObject[] g;
+
+	// Use this for initialization
+	void Start () {
+
+		g = GameObject.FindGameObjectsWithTag ("attachmentObject"); 	
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		//int i = 0;
+		//while( i < objects.Count) {
+		int i = 0;
+			instrM = g [i].GetComponent<Instr_Manual> ();
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				instrM.start = true;
+			}
+
+
+		if (g [i].transform.position == instrM.target.position) {
+			i++;
+			instrM = g [i].GetComponent<Instr_Manual> ();
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				instrM.start = true;
+			}
+		}
+
+		if (g [i].transform.position == instrM.target.position) {
+			i++;
+			instrM = g [i].GetComponent<Instr_Manual> ();
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				instrM.start = true;
+			}
+		}
+	}
+}
